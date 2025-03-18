667b2ba002c7870838455c182752ae10b01a0594
samki <samialdobai23@gmail.com>
2025-03-18 03:57:51 +0300

    اخر عمل

--- University/ViewModels/LoginViewModel.cs
+++ University/ViewModels/LoginViewModel.cs
@@ -4,10 +4,13 @@ namespace University.ViewModels
 {
     public class LoginViewModel
     {
-        [Required]
-        public string UserName { get; set; }  
-        [Required]
+        [Required(ErrorMessage = "User Name is required")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
+
         public bool RememberMe { get; set; }
     }
 }
